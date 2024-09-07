9614d9a4c322ee0f19fcfabafa6df087583c9bee
Martin A. Ortega <lynxestudio@outlook.com>
2024-09-06 22:21:10 -0600

    Adds source code to the project

---
+++ src/Server/Samples.GradeConverterServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Samples.GradeConverterService;
+using System.ServiceModel;
+
+namespace Samples.GradeConverterServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type serviceImplementation = typeof(GradeConverterImplementation);
+            var address = new Uri("http://localhost:5388/");
+            ServiceHost host = new ServiceHost(serviceImplementation, address);
+            using (host)
+            {
+                host.Open();
+                Console.WriteLine("Grade converter service running. Press [Enter] to quit.");
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
